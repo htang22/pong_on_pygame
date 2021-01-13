5a6bf033446c58e62f0a64b4c0335ef8a79a7f19
Hong Bing <hbtang2004@gmail.com>
2021-01-13 13:51:54 -0500

    Create the paddles
    Created the paddle and paddle class and then created a left and right paddle object. Then I made a rectangle on the screen in the main loop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import display
+import paddle
 
 pygame.init()
 
@@ -8,11 +9,18 @@ pygame.display.set_caption("Pong")
 
 display = display.Display()
 screen = display.screen
+left_paddle = paddle.Paddle("left")
+right_paddle = paddle.Paddle("right")
+
 
 running = True
 while running:
     pygame.time.delay(100)
+    pygame.draw.rect(screen, right_paddle.color,
+                     (right_paddle.x_cord, right_paddle.y_cord, right_paddle.width, right_paddle.height))
+    pygame.draw.rect(screen, left_paddle.color, (left_paddle.x_cord, left_paddle.y_cord, left_paddle.width, left_paddle.height))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    pygame.display.update()
 pygame.quit()
