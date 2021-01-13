08c78fbf9d74e8a8d09288787c71eed95a0a21f6
Hong Bing <hbtang2004@gmail.com>
2021-01-13 17:02:30 -0500

    Paddle movement
    If click w or s would move the left paddle up or down and if you click the arrow up and down keys would move the right paddle up or down.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import display
 import paddle
 
 pygame.init()
-
+y_speed = 15
 # screen = pygame.display.set_mode(size=(600,600))
 pygame.display.set_caption("Pong")
 
@@ -16,11 +16,26 @@ right_paddle = paddle.Paddle("right")
 running = True
 while running:
     pygame.time.delay(100)
+    screen.fill((0, 0, 0))
     pygame.draw.rect(screen, right_paddle.color,
                      (right_paddle.x_cord, right_paddle.y_cord, right_paddle.width, right_paddle.height))
     pygame.draw.rect(screen, left_paddle.color, (left_paddle.x_cord, left_paddle.y_cord, left_paddle.width, left_paddle.height))
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_UP] and right_paddle.y_cord > 0:
+        right_paddle.y_cord -= y_speed
+    if keys[pygame.K_DOWN] and right_paddle.y_cord < 600 - right_paddle.height:
+        right_paddle.y_cord += y_speed
+    if keys[pygame.K_w] and left_paddle.y_cord > 0:
+        left_paddle.y_cord -= y_speed
+    if keys[pygame.K_s] and left_paddle.y_cord < 600 - left_paddle.height:
+        left_paddle.y_cord += y_speed
+
+        
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
     pygame.display.update()
 pygame.quit()
