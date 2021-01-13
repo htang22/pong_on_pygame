783953f1871b80c191324029f04f56c7493f8181
Hong Bing <hbtang2004@gmail.com>
2021-01-13 12:46:45 -0500

    Display and loop
    Added the display and the main loop to the main.py file

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import pygame
+import display
 
 pygame.init()
+
+# screen = pygame.display.set_mode(size=(600,600))
+pygame.display.set_caption("Pong")
+
+display = display.Display()
+screen = display.screen
+
+running = True
+while running:
+    pygame.time.delay(100)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
