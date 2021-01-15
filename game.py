ece94a3e5d8f9242b6b4667272aad28680641cb8
Hong Bing <hbtang2004@gmail.com>
2021-01-15 14:26:26 -0500

    Gameover
    rest of the game over stuff I forgot to commit

--- game.py
+++ game.py
@@ -24,6 +24,17 @@ class Game:
         left_pad = pygame.draw.rect(self.screen, self.left_paddle.color,
                                     (self.left_paddle.x_cord, self.left_paddle.y_cord, self.left_paddle.width, self.left_paddle.height))
 
+
+        if keys[pygame.K_UP] and self.right_paddle.y_cord > 0:
+            self.right_paddle.y_cord -= self.y_speed
+        if keys[pygame.K_DOWN] and self.right_paddle.y_cord < 600 - self.right_paddle.height:
+            self.right_paddle.y_cord += self.y_speed
+        if keys[pygame.K_w] and self.left_paddle.y_cord > 0:
+            self.left_paddle.y_cord -= self.y_speed
+        if keys[pygame.K_s] and self.left_paddle.y_cord < 600 - self.left_paddle.height:
+            self.left_paddle.y_cord += self.y_speed
+
+
         # Drawing the ball and ball movement
         game_ball = pygame.draw.rect(self.screen, self.ball.color, (self.ball.x_cord, self.ball.y_cord, self.ball.width, self.ball.height))
 
@@ -52,30 +63,29 @@ class Game:
             self.ball.ball_moving_state = False
             if self.ball.x_cord < 30:
                 self.score.right_score += 1
-                self.score.show_score(self.screen)
                 self.ball.ball_x_speed = 5
             elif self.ball.x_cord > 770:
                 self.score.left_score += 1
-                self.score.show_score(self.screen)
                 self.ball.ball_x_speed = 5
                 self.ball.ball_x_speed *= -1
             self.ball.reset_ball()
 
 
         # score
-        self.score.show_score(self.screen)
+        self.score.show_score(self.screen, (255, 255, 255))
+
+
 
         # Game over
-        self.score.game_over()
+        if self.score.game_over():
+            self.screen.fill((0, 0, 0))
+            self.score.reset_score()
+
+            return False
+        else:
+            return True
 
         # Checks to see if a key is pressed if one is then move a paddle
 
 
-        if keys[pygame.K_UP] and self.right_paddle.y_cord > 0:
-            self.right_paddle.y_cord -= self.y_speed
-        if keys[pygame.K_DOWN] and self.right_paddle.y_cord < 600 - self.right_paddle.height:
-            self.right_paddle.y_cord += self.y_speed
-        if keys[pygame.K_w] and self.left_paddle.y_cord > 0:
-            self.left_paddle.y_cord -= self.y_speed
-        if keys[pygame.K_s] and self.left_paddle.y_cord < 600 - self.left_paddle.height:
-            self.left_paddle.y_cord += self.y_speed
+
--- score.py
+++ score.py
@@ -9,18 +9,25 @@ class Score:
         self.right_cord = (465, 50)
         self.left_cord = (300, 50)
 
-    def show_score(self,window):
+    def show_score(self,window, color):
         font = pygame.font.Font("freesansbold.ttf", 80)
-        right_score = font.render(f"{self.right_score}", True, (255, 255, 255))
-        left_score = font.render(f"{self.left_score}", True, (255, 255, 255))
-        pygame.draw.rect(window, (255, 255, 255), (400, 0, 10, 275))
-        pygame.draw.rect(window, (255, 255, 255), (400, 350, 10, 275))
+        right_score = font.render(f"{self.right_score}", True, color)
+        left_score = font.render(f"{self.left_score}", True, color)
+        pygame.draw.rect(window, color, (400, 0, 10, 275))
+        pygame.draw.rect(window, color, (400, 350, 10, 275))
         window.blit(right_score, self.right_cord)
         window.blit(left_score, self.left_cord)
 
     def game_over(self):
         if self.left_score == 7 or self.right_score == 7:
-            return 0
+            return True
+        else:
+            return False
+    def reset_score(self):
+        self.right_score = 0
+        self.left_score = 0
+
+
 
 
 
